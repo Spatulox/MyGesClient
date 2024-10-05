1d3178c9e1ad1d3f9da266fae2d96f43d5e470e5
Spatulox <M78stormtrooper@laposte.net>
2024-10-05 17:39:52 +0200

    update index_events.js
    add functionnalities

--- frontend/src/JS/index_events.js
+++ frontend/src/JS/index_events.js
@@ -1,18 +1,70 @@
+
+// ------------------------------------------------------------------ //
+// Functions
+
+function loadPage(string){ // Used in index.html
+    loadPageGo(string)
+}
+
+// ------------------------------------------------------------------ //
+// Listener
 const lightDark = document.getElementById('theme');
 const body = document.getElementsByTagName('body')[0]
+const buttonEula = document.getElementById('buttonEula')
 
+const buttonConnection = document.getElementById('buttonConnection')
+const buttonCancelConnection = document.getElementById('buttonCancelConnection')
+
+
+
+// ------------ Utilitites events -------------- //
 lightDark.addEventListener('click', function() {
 
     if(body.classList.contains('light')){
         lightDark.src = "./src/assets/images/black-moon.png"
-        //replaceValueJsonFile("./config.json", "theme", "dark")
+        UpdateUserTheme("dark")
     }
     else{
         lightDark.src = "./src/assets/images/black-sun.png"
-        //replaceValueJsonFile("./config.json", "theme", "light")
+        UpdateUserTheme("light")
     }
 
     body.classList.toggle('light')
     body.classList.toggle('dark')
 
 });
+
+buttonEula.addEventListener('click', function() {
+    const eula = document.getElementById('eula')
+    UpdateUserEula()
+    eula.classList.remove('active')
+})
+
+buttonCancelConnection.addEventListener('click', function() {
+    const connection = document.getElementById('connection')
+    connection.classList.remove("active")
+
+})
+
+
+// ------------ Popup events -------------- //
+
+async function eulaShow(){
+    const eula = document.getElementById('eula')
+    eula.classList.add('active')
+}
+
+async function changeLoginPassword(){
+    //const eula = document.getElementById('eula')
+    const connection = document.getElementById('connection')
+    connection.classList.add('active')
+}
+
+async function deconnectionFromMyges(){
+    loadPage("cya")
+    changeLoginPassword()
+}
+
+async function deleteOldData(){
+    popup("Under contruction")
+}
