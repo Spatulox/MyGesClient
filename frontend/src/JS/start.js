c3db2cd77f8028783bcaad8e7ddd06756a2b775f
Spatulox <M78stormtrooper@laposte.net>
2024-10-05 17:39:25 +0200

    updaet start.js
    add functionnalites

--- frontend/src/JS/start.js
+++ frontend/src/JS/start.js
@@ -1,40 +1,62 @@
-import { GetUserData } from "../../wailsjs/go/main/App";
+import { GetUserData, InitDiscordRPC, UpdateDiscordRPC } from "../../wailsjs/go/backend/App";
+import { loadPageGo } from "./loadPages";
 
 async function start(){
 
     try {
         const user = await GetUserData();
-        console.log(user)
         if (user === null || user.password) {
             const connection = document.getElementById('connection')
             connection.classList.add('active')
         }
+        // Check if the eula is accepted
+        if(!user.EULA || user.EULA === false)
+        {
+            const eula = document.getElementById('eula')
+            eula.classList.add('active')
+        }
+
+        // Apply the theme
+        const theme = document.getElementsByTagName('body')[0]
+        const themeImg = document.getElementById('theme')
+        if(user.Theme){
+            theme.classList = user.Theme
+        }
+
+        if(theme.classList.contains('light')){
+            themeImg.src = './src/assets/images/black-sun.png'
+        }
+        else{
+            themeImg.src = './src/assets/images/black-moon.png'
+        }
+
+        connectDiscord()
+        loadPageGo("dashboard.html")
+
     } catch (error) {
         console.error("Erreur lors de la récupération des données utilisateur :", error);
     }
-    /*
-    // Check if the eula is accepted
-    if(!user.eula || user.eula === false)
-    {
-        const eula = document.getElementById('eula')
-        eula.classList.add('active')
-    }
 
-    // Apply the theme
-    const theme = document.getElementsByTagName('body')[0]
-    const themeImg = document.getElementById('theme')
-    if(user.theme){
-        theme.classList = user.theme
-    }
 
-    if(theme.classList.contains('light')){
-        themeImg.src = './src/assets/images/black-sun.png'
-    }
-    else{
-        themeImg.src = './src/assets/images/black-moon.png'
-    }*/
 
 }
+
+async function connectDiscord(){
+        // Initialiser Discord RPC
+    InitDiscordRPC()
+        .then(() => console.log("Discord RPC initialized"))
+        .catch(error => console.error("Failed to initialize Discord RPC:", error));
+
+}
+
+function updateActivity(state, details) {
+    UpdateDiscordRPC(state, details)
+        .then(() => console.log("Discord RPC activity updated"))
+        .catch(error => console.error("Failed to update Discord RPC activity:", error));
+}
+
+
 start()
+
 
 
