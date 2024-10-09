b6b48574d4f40eae9553e9f5c9bfa5101d1a2e87
Spatulox <M78stormtrooper@laposte.net>
2024-10-09 18:42:27 +0200

    update gradesDB add message

--- db/gradesDB.go
+++ db/gradesDB.go
@@ -2,11 +2,16 @@ package db
 
 import "database/sql"
 import . "MyGesClient/structures"
+import . "MyGesClient/log"
 
 func SaveGradesToDB(grades string, db *sql.DB) {
-
+	Log.Infos("INFO : Saving grades into local DB")
+	print(grades)
+	Log.Infos("INFO : Grades saved into local DB")
 }
 
 func GetDBUserGrades(year string, db *sql.DB) ([]Grades, error) {
+	Log.Infos("INFO : Fetching local DB for grades")
+	Log.Infos("INFO : Fetched Grades")
 	return nil, nil
 }
