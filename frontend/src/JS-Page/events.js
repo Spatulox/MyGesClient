c1872ed67c4dea4f2ba25e2c8cd5c63506f238ea
Spatulox <M78stormtrooper@laposte.net>
2024-10-19 16:20:28 +0200

    fix events refreshing when searching for a specific events

--- frontend/src/JS-Page/events.js
+++ frontend/src/JS-Page/events.js
@@ -2,8 +2,13 @@ import {GetAllEvents, GetEvents, GetEventsLike, SaveEvents} from "../../wailsjs/
 import {formatDateWithDay, scrollMainPart} from "../JS/functions";
 let personnalEvents
 let displayEventsId = []
+let specificEvent = false
 export async function events(){
 
+    if(specificEvent){
+        console.log("stopEvent")
+        return
+    }
     scrollMainPart()
 
     const searchBar = document.getElementById("searchBar")
@@ -18,10 +23,13 @@ export async function events(){
     populateCalendar(personnalEvents);
 
     searchBar.addEventListener("input", async()=>{
+        if(searchBar.value === ""){
+            specificEvent = false
+        } else {
+            specificEvent = true
+        }
         const searchedEvents = await GetEventsLike(searchBar.value)
         populateCalendar(searchedEvents)
-        console.log(searchBar.value)
-        console.log(searchedEvents)
     })
 
     if(displayEventsId.length === 0){
